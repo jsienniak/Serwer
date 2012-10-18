19efa78922365cc36bad701af4b1b4a532a148f7
mm <mm@localhost.localdomain>
2012-10-18 18:29:32 +0100

    Initial

---
+++ server/src/pl/zpi/server/ServTest.java
@@ -0,0 +1,108 @@
+package pl.zpi.server;
+
+import java.io.*;
+import java.util.Arrays;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.xerces.parsers.XML11Configuration;
+
+
+import pl.zpi.server._trash.ITokenResolverClassLoader;
+import pl.zpi.server._trash.TokenReplacingReader;
+import pl.zpi.server.db.DBUsers;
+
+// Extend HttpServlet class
+public class ServTest extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2904860740383334172L;
+	private String message;
+	public static String appPath = "";
+	DBUsers o;
+
+	public void init() throws ServletException {
+		// Do required initialization
+
+		message = "Hello World";
+		appPath = getServletContext().getRealPath("/");
+		try {
+			Class.forName("org.postgresql.Driver").getClass();
+		} catch (ClassNotFoundException e) {
+			message = "No driver";
+			e.printStackTrace();
+		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, FileNotFoundException {
+	
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.println("Test servera");
+		
+		FileInputStream fin = new FileInputStream(new File(appPath+"test.xml"));
+		TokenReplacingReader trr = new TokenReplacingReader(new InputStreamReader(fin), new ITokenResolverClassLoader());
+		/*String s = fin.read
+
+		PipedOutputStream pout = new PipedOutputStream();
+		pout.write();*/
+		DBUsers obj = new DBUsers();
+		
+		obj.set("login", "nowy");
+		obj.write();
+		// Actual logic goes here.
+		// Use the static TransformerFactory.newInstance() method to instantiate
+		// a TransformerFactory. The javax.xml.transform.TransformerFactory
+		// system property setting determines the actual class to instantiate --
+		// org.apache.xalan.transformer.TransformerImpl.
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+
+		// Use the TransformerFactory to instantiate a Transformer that will
+		// work with
+		// the stylesheet you specify. This method call also processes the
+		// stylesheet
+		// into a compiled Templates object.
+		Transformer transformer = null;
+		
+		try {
+			transformer = tFactory.newTransformer(new StreamSource(appPath+"test.xsl"));
+		} catch (TransformerConfigurationException e1) {
+			out.println(Arrays.toString(e1.getStackTrace()));
+			e1.printStackTrace();
+		}
+
+		// Use the Transformer to apply the associated Templates object to an
+		// XML document
+		// (foo.xml) and write the output to a file (foo.out).
+		try {
+			if (transformer != null) {
+				transformer.transform(new StreamSource(trr), new StreamResult(out));
+			}
+		} catch (TransformerException e) {
+			out.println(Arrays.toString(e.getStackTrace()));
+			e.printStackTrace();
+		}
+
+	/*	System.out.println("************* The result is in birds.out *************");
+		DatabaseObject da = new DatabaseObject();
+		Vector v = da.getObjects();
+		Iterator it = v.iterator();
+		while (it.hasNext()) {
+			out.println(it.next().toString() + "<br />");
+		}*/
+
+	}
+
+	public void destroy() {
+		// do nothing.
+	}
+}
---
+++ server/web/test.xsl
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:template match="/">
+		<html>
+			<body>
+				<table>
+				<tr>
+				<td>KLEJ</td>
+				</tr>
+				</table>
+
+			</body>
+		</html>
+	</xsl:template>
+</xsl:stylesheet>
