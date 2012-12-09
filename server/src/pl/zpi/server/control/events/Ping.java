e9960c89f09cb6e92cda7c785c12b5ae1e452be7
mm <mm@ip9220.t16.ds.pwr.wroc.pl>
2012-12-09 23:38:10 +0100

    d

--- server/src/pl/zpi/server/control/events/Ping.java
+++ server/src/pl/zpi/server/control/events/Ping.java
@@ -2,12 +2,19 @@ package pl.zpi.server.control.events;
 
 import static pl.zpi.server.utils.XMLToolkit.createTextNode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 import pl.zpi.server.control.Event;
+import pl.zpi.server.db.DBDevices;
+import pl.zpi.server.db.DatabaseObj;
+import pl.zpi.server.db.DatabaseObjImpl;
 
 public class Ping extends Event{
 	
@@ -16,12 +23,19 @@ public class Ping extends Event{
 		Node r = doc.createElement("ping");
 		r.appendChild(createTextNode(doc, "status", "success"));
 		r.appendChild(createTextNode(doc, "message","PONG"));
+		DBDevices dev = new DBDevices();
+        Vector<DatabaseObj> devices = dev.executeQuery();
+        List<String> ID  = new ArrayList<String>();
+        for(DatabaseObj ob: devices){
+            ID.add(ob.get("reg_id"));
+            System.out.println(ob.getStringNotNull("reg_id"));
+        }
 		return r;
 	}
 
 	@Override
 	public String getName() {
-		return "module.ping";
+		return "m.ping";
 	}
 
 }
--- server/src/pl/zpi/server/db/DBDevices.java
+++ server/src/pl/zpi/server/db/DBDevices.java
@@ -1,36 +1,26 @@
 package pl.zpi.server.db;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
 
 public class DBDevices extends DatabaseObjImpl{
 
-	protected static String[] columnNames = null;
-
-	private static final Logger logger = Logger.getLogger(DBDevices.class.getName());
-
-	public DBDevices(int ID) {
-		this();
-		this.objectID = ID;
+	public DBDevices(){
+		super(); 
+		if(columnNames == null){
+			getColumnNames();
+		}
 	}
-
-	public DBDevices() {
-		super();
+	@Override
+	public void config(){
 		this.primaryKey = "id_device";
 		this.tableName = "devices";
-		data = new HashMap<String, String>();
+	}
+    public DBDevices(int i) {
+		super(i);
 	}
 
-	public DBDevices(Map data) {
-		this();
-		this.data = data;
-	}
-
-	public DBDevices(String table, String primaryKey) {
-		data = new HashMap<String, String>();
-		this.primaryKey = primaryKey;
-		this.tableName = table;
+	@Override
+	protected synchronized void getColumnNames(){
+		super.getColumnNames();
 	}
 
 }
